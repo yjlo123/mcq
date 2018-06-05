f1700b4399f5f7b76265ff43a84a7817bb5769d8
yjlo123 <liusiwei.yjlo@gmail.com>
2018-06-06 00:47:31 +0800

    Initial push

---
+++ data.js
@@ -0,0 +1,19 @@
+var qa = {
+"qa": [
+	{
+		"Q": "What type of animal is a seahorse?",
+		"O": ["Crustacean", "Arachnid", "Fish", "Shell"],
+		"A": 0
+	},
+	{
+		"Q": "What color are zebras?",
+		"O": ["White with black stripes", "Black with white stripes", "Both of the above", "None of the above"],
+		"A": 1
+	},
+	{
+		"Q": "What animal lives the longest?",
+		"O": ["Ocean quahog (clam)", "Red sea urchin", "Galapagos tortois"],
+		"A": 0
+	}
+]
+};
